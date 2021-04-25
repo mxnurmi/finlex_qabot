edbb8158a20e2dfc62b0bd38a2754476c6cdfe45
Miska <miska.nurmi@helsinki.fi>
2021-04-25 13:47:42 +0300

    First working version done

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
-#import sparql
 #%%
 from SPARQLWrapper import SPARQLWrapper, JSON
 import whoosh.index as index
-from whoosh import index
+from whoosh import qparser
 from whoosh.fields import Schema, TEXT
 from whoosh.qparser import QueryParser
 from whoosh.analysis import LanguageAnalyzer
 import os.path
-# https://pypi.org/project/sparql-client/
 import re
 
 #%%
 
-endpoint = "http://data.finlex.fi/sparql"
-
-def fetchLawData(q, endpoint):
+def fetchLawData(q, endpoint="http://data.finlex.fi/sparql"):
     #res = sparql.Service(endpoint, qs_encoding='utf-8').query(q)
     #data = []
     #variables = (res.variables)
@@ -31,26 +27,21 @@ def fetchLawData(q, endpoint):
     
     try:
         jsondata = sparql.query().convert()
-        
-        print(jsondata)
-        
+        #print(jsondata)
         for result in jsondata["results"]["bindings"]:
             item = result["content"]["value"]
-            #item = item.encode('utf-8').strip()
             text.append(item)
     except Exception as e:
-        print("The data fetch failed", e)
+        print("The sparkql data fetch failed", e)
         
-    
     #Return only the latest item TODO: better way would be to only fetch the latest?
     lex = text[-1]
       
     return lex
 
-
 # if __name__ == "__main__":
 
-#     #NOTE: spaces in the queries are super important for them to work!!
+#     #NOTE: spaces in the queries change the querie so be careful with them
 #     q2 = ("""
 #       PREFIX sfl: <http://data.finlex.fi/schema/sfl/>
 #       PREFIX eli: <http://data.europa.eu/eli/ontology#>
@@ -82,30 +73,29 @@ def fetchLawData(q, endpoint):
 
 # %%
 
-q2 = ("""
-    PREFIX sfl: <http://data.finlex.fi/schema/sfl/>
-    PREFIX eli: <http://data.europa.eu/eli/ontology#>
+# q1 = ("""
+#     PREFIX sfl: <http://data.finlex.fi/schema/sfl/>
+#     PREFIX eli: <http://data.europa.eu/eli/ontology#>
     
-    # Query : Get different temporal versions of Criminal Code
-    SELECT ?document_version ?format ?content 
-    WHERE {
-    # you can replace <http://data.finlex.fi/eli/sd/1889/39> with eg. one of the following:
-    # http://data.finlex.fi/eli/sd/1889/39/johtolause
-    # http://data.finlex.fi/eli/sd/1889/39/luku/1
-    # http://data.finlex.fi/eli/sd/1889/39/luku/1/pykala/1
-    <http://data.finlex.fi/eli/sd/1889/39> eli:has_member ?document_version .
+#     # Query : Get different temporal versions of Criminal Code
+#     SELECT ?document_version ?format ?content 
+#     WHERE {
+#     # you can replace <http://data.finlex.fi/eli/sd/1889/39> with eg. one of the following:
+#     # http://data.finlex.fi/eli/sd/1889/39/johtolause
+#     # http://data.finlex.fi/eli/sd/1889/39/luku/1
+#     # http://data.finlex.fi/eli/sd/1889/39/luku/1/pykala/1
+#     <http://data.finlex.fi/eli/sd/1889/39> eli:has_member ?document_version .
     
-    # lang options: fin,swe
-    # format options: txt and html
-    BIND(IRI(CONCAT(str(?document_version),"/fin/txt")) AS ?format)
+#     # lang options: fin,swe
+#     # format options: txt and html
+#     BIND(IRI(CONCAT(str(?document_version),"/swe/txt")) AS ?format)
     
-    # replace sfl:text with sfl:html for html version
-    ?format sfl:text ?content.}
-    """)
+#     # replace sfl:text with sfl:html for html version
+#     ?format sfl:text ?content.}
+#     """)
 
-lex = fetchLawData(q2, endpoint)
+# lex = fetchLawData(q1, endpoint)
 
-# %%
 
 def preprocessLawText(txt):
     
@@ -125,9 +115,6 @@ def preprocessLawText(txt):
 
     return final_txt
 
-preprosessed_lex = preprocessLawText(lex)
-#print(preprosessed_lex[0])
-
 # %%
 
 def indexLawText(law_as_list):
@@ -141,15 +128,15 @@ def indexLawText(law_as_list):
     #TODO:use writer.add_document() to add each pykala per luku
     # e.g. writer.add_document(content=u"Luku 1")
 
-    stem_ana = LanguageAnalyzer("fi") #TODO: should be LanguageAnalyzer("es") ('ar','da','nl','en','fi','fr','de','hu','it','no','pt','ro','ru','es','sv','tr')
-    schema = Schema(content=TEXT(analyzer=stem_ana, stored=True))
+    ana = LanguageAnalyzer("sv") #TODO: should be LanguageAnalyzer("es") ('ar','da','nl','en','fi','fr','de','hu','it','no','pt','ro','ru','es','sv','tr')
+    schema = Schema(content=TEXT(analyzer=ana, stored=True))
 
     if not os.path.exists("index"):
         os.mkdir("index")
 
     if index.exists_in("index"):
-        print("overwriting old index")
-
+        print("Overwriting old index...")
+    
     ix = index.create_in("index", schema)
 
     ix = index.open_dir("index")
@@ -160,10 +147,8 @@ def indexLawText(law_as_list):
         writer.add_document(content=seg)
     
     writer.commit(optimize=True)
-
+    print("Indexing done. You can now use the searchTool() for your queries")
     return ix
-
-ix = indexLawText(preprosessed_lex)
 
 # %%
 
@@ -172,18 +157,30 @@ def searchFromIndexedLaw(ix, querystr):
     Search query from the indexed law text
 
     Parameters:
-    ix: Index object that contains info about the indexed text
+    ix: Index object that contains info about the indexed text; if None, uses "./index"
     querystr: Search query as String
 
     """
+    if ix == None:
+        if index.exists_in("index"):
+            ix = index.open_dir("index")
+        else: 
+            raise Exception("The index folder is missing, initialize it with initSearchTool() first")
 
-    qp = QueryParser("content", schema=ix.schema)
-    query = qp.parse(querystr)
+    og = qparser.OrGroup.factory(0.5)
+    qp = QueryParser("content", schema=ix.schema, group=og)
+
+    query_parsed = ""
+    ana = LanguageAnalyzer("sv") 
+    for token in ana(querystr):
+        query_parsed += " " + token.text
+    
+    query = qp.parse(query_parsed)
 
     topAnswers = []
 
     with ix.searcher() as searcher:
-        results = searcher.search(query, limit=20)
+        results = searcher.search(query, limit=10)
         
         for item in results:
             topAnswers.append(item["content"])
@@ -193,9 +190,59 @@ def searchFromIndexedLaw(ix, querystr):
 
     return topAnswers
 
-answers = searchFromIndexedLaw(ix=ix, querystr="kuolema") #TODO: one issue is with queries with lemma / stemma e.g. murha vs murhasta
 
-print(answers[0:4])
+def initSearchTool(query=None):
+    """
+    Initialize the search tool. Fetches and 
 
+    Parameters:
+    ix: Index object that contains info about the indexed text; if None, uses "./index"
+    querystr: Search query as String
+
+    """
+
+    if (query==None):
+        q = ("""
+            PREFIX sfl: <http://data.finlex.fi/schema/sfl/>
+            PREFIX eli: <http://data.europa.eu/eli/ontology#>
+            
+            # Query : Get different temporal versions of Criminal Code
+            SELECT ?document_version ?format ?content 
+            WHERE {
+            # you can replace <http://data.finlex.fi/eli/sd/1889/39> with eg. one of the following:
+            # http://data.finlex.fi/eli/sd/1889/39/johtolause
+            # http://data.finlex.fi/eli/sd/1889/39/luku/1
+            # http://data.finlex.fi/eli/sd/1889/39/luku/1/pykala/1
+            <http://data.finlex.fi/eli/sd/1889/39> eli:has_member ?document_version .
+            
+            # lang options: fin,swe
+            # format options: txt and html
+            BIND(IRI(CONCAT(str(?document_version),"/swe/txt")) AS ?format)
+            
+            # replace sfl:text with sfl:html for html version
+            ?format sfl:text ?content.}
+            """)
+    else:
+        q = query
+
+    lex = fetchLawData(q)
+    preprosessed_lex = preprocessLawText(lex)
+    ix = indexLawText(preprosessed_lex)
+
+def searchTool(search_query):
+
+    answers = searchFromIndexedLaw(ix=None, querystr=search_query)
+
+    #for i,answer in enumerate(answers):
+        #if i == 5:
+            #break
+        #print("Answer number %i: %s" % (i+1,answer))
+        #print("")
+
+    return answers
 
 # %%
+#initSearchTool()
+
+#x = searchTool("Vilken typ av dom förskingring kan fördomas?")
+#print(x[0])
